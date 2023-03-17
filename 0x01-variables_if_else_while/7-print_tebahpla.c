3e2a02a9bfb92836dd02f2f41dd3f03aa25430c1
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-17 11:27:56 -0700

    print reverse small letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints small letters in reverse order
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
