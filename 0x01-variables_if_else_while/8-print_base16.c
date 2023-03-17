a54a730b8093cafee13d0ac2da264a46d182b0fe
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-17 11:35:56 -0700

    print base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print base 16 numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + n);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
