3b04319e6f9286edbec3524297c7aaf983e101fe
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-17 10:27:17 -0700

    print small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - it prints small letters
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
