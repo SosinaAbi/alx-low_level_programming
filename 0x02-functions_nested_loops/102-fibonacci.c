39af1fa75353682a1f0116b3c31ac16ab0f59c89
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-21 12:23:23 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int f1 = 1;
+	int f2 = 1;
+	int fib = 0;
+	int n;
+
+	printf("%d, %d, ", f1, f2);
+	for (n = 3; n <= 50; n++)
+	{
+		fib = f2 + f1;
+		printf("%d, ", fib);
+
+		f1 = f2;
+		f2 = fib;
+	}
+	printf("\n");
+}
