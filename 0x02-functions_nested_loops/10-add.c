44922dcd96e3ea1e675b2df5ce3561f9fa50c867
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-21 09:02:02 -0700

    addition result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,21 +7,6 @@
  */
 int add(int x, int y)
 {
-	int sum = x + y;
-	int d = 10;
-
-	while ((sum / d) > 9)
-	{
-		d = d * 10;
-	}
-
-	while (d > 10)
-	{
-		_putchar((sum / d) + '0');
-		d = d / 10;
-	}
-	_putchar((sum % d) + '0');
-	_putchar('\n');
 	return (x + y);
 }
 
