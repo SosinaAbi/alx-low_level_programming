aafaad5ca4aea6f625e49bb9284ac58e36b06491
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-21 07:16:16 -0700

    24 houres

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int z = 0;
+
+	for (int y = 0; y < 24; y++)
+	{
+		for (x = 0; x < 60; x++)
+		{
+			z++;
+		}
+	}
+
+	_putchar('0' + z);
+}
