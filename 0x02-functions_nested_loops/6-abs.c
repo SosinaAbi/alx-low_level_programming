813a086aabf101bf1fee44cae4c8b226e9fa78a5
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-21 06:08:17 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @int: integer parameter
+ * Return: returns positive number
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		return ((n * (-1)));
+	}
+	else
+		return (n);
+}
