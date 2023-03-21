349761a2b6b4a354dbc09905dd9a56a0abfeea60
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-21 03:00:39 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 /**
  * _islower - Tells small or capital letters
  * Return: 1 if lower 0 if not
+ * c: int parameter
  */
 int _islower(int c)
 {
-	int c;
+	int ch;
 
-	if ((c >= 'A') && (c <= 'Z'))
+	if ((ch >= 'A') && (ch <= 'Z'))
 		return (0);
 	else
 		return (1);
