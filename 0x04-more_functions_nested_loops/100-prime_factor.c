aefae4cc7c68d1f212f82ad1fe3537b41aaa827d
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-23 08:30:44 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor of a number
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	long x, y;
+	int i;
+
+	x = 612852475143;
+	y = x;
+
+	while ((y % 2 == 0) || (y % 3 == 0))
+	{
+		if (y % 2 == 0)
+		{
+			y = y / 2;
+		}
+		else if (y % 3 == 0)
+		{
+			y = y / 3;
+		}
+	}
+
+	printf("%ld\n", y);
+	return (0);
+}
