a7860e5702c4a83875ce7da79518d0bc1b4d64b3
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-27 00:30:12 -0700

    string reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses the character array
+ * @s: character parameter
+ */
+
+void rev_string(char *s)
+{
+	int count, i, j;
+
+	count = 0;
+	i = 0;
+	j = 0;
+
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+
+	j = count - 1;
+	i = 0;
+
+	while (j >= 0)
+	{
+		s[i] = s[j];
+		i++;
+		j--;
+	}
+}
+
+
