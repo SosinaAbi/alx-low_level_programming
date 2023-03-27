745729e976142d19b6b2bc1be726dcf557b9470d
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-27 07:32:32 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,27 +8,21 @@
 void rev_string(char *s)
 {
 	int count, i, j;
+	char hold;
 
 	count = 0;
 	i = 0;
-	j = 0;
 
 	while (s[i] != '\0')
 	{
 		count++;
 		i++;
 	}
-	char h[count];
 
-	h[count] = s[count];
-	j = count - 1;
-	i = 0;
-
-	while (j >= 0)
+	for (j = count - 1; j >= count / 2; j++)
 	{
-		s[i] = h[j];
-		i++;
-		j--;
+		hold = s[j];
+		s[j] = s[count - j - 1];
+		s[count - j - 1] = hold;
 	}
-
 }
