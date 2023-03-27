96b4b80b84f96a4fe735719afbbd836b6c6686ab
SosinaAbi <sosinaabiyot@gmail.com>
2023-03-27 09:37:37 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,23 +3,22 @@
 /**
  * _atoi - prints numbers from character array
  * @s: character array
- *
  * Return: integer
  */
 
 int _atoi(char *s)
 {
 	int pos, neg;
-	int count, i;
+	int count, i, j;
+	int num;
 
 	pos = 0;
 	neg = 0;
 	count = 0;
 	i = 0;
-
+	j = 0;
 	while (s[i++])
 		count++;
-
 	for (i = 0; i < count; i++)
 	{
 		if (s[i] == '+')
@@ -27,15 +26,18 @@ int _atoi(char *s)
 		else if (s[i] == '-')
 			neg++;
 	}
-
 	if (neg > pos)
-		_putchar('-');
+	{
+		num[j] = '-';
+		j++;
+	}
 	for (i = 0; i < count; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
-			_putchar(s[i]);
-		else
-			continue;
+		{
+			num[j] = s[i];
+			j++;
+		}
 	}
-	_putchar('\n');
+	return (num);
 }
