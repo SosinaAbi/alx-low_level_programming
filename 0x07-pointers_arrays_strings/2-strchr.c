80468c1520ed80d025d30fab6202374e0f6f82ed
SosinaAbi <sosinaabiyot@gmail.com>
2023-04-04 13:32:06 +0000

    character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Checks if character c occurs in s.
+ * @s: Character array.
+ * @c: Character to be checked.
+ *
+ * Return: Returns pointer to first occurence of c
+ * in the string s, or NULL if c is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0, n = 0;
+
+	while (s[i++])
+		n++;
+
+	for (i = 0; i < n; i++)
+	{
+		if (c == s[i])
+			return (s);
+		else
+			return (NULL);
+	}
+}
