7624fbdf3d6bb6eb9cc91687c1f758b58b2f8ad3
SosinaAbi <sosinaabiyot@gmail.com>
2023-04-04 13:10:01 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* memset - Saves b in s arrays for n bytes.
-* @s: Destination array.
-* @b: Data to be saved in s.
-* @n: Number of bytes for array s.
-*
-* Return: Returns array s.
-*/
+ * memset - Saves b in s arrays for n bytes.
+ * @s: Destination array.
+ * @b: Data to be saved in s.
+ * @n: Number of bytes for array s.
+ *
+ * Return: Returns array s.
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
