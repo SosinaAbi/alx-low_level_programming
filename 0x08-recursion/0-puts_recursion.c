b6ca18abadc7118a2296f0acb6cf3c1125891e1d
SosinaAbi <sosinaabiyot@gmail>
2023-04-05 12:53:38 +0000

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
-		_putchar(s[i]);
-
-	else
+	if (*s == '\0')
+	{
 		_putchar('\n');
-	i++;
+		return;
+	}
+	_putchar(*s);
+	s++;
 	_puts_recursion(s);
 }
