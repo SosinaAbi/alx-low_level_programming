e1fafb65f373c328a5f185d38e6422182b59b02a
SosinaAbi <sosinaabiyot@gmail>
2023-04-08 12:11:52 +0000

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints program name.
+ * @argc: The size of the argv array
+ * @argv: String array of size argc
+ *
+ * Return: 0 is successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
