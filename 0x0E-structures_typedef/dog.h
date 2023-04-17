8d42ac589a6a5e1e64c755e1efc7eda42cc15046
SosinaAbi <sosinaabiyot@gmail.com>
2023-04-17 03:21:17 -0700

    Dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Contains dog structure.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Containes name of the owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
