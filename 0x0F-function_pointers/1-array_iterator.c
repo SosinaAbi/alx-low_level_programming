82db67704ffcf801efdba56edf851582540c76e4
SosinaAbi <sosinaabiyot@gmail.com>
2023-04-19 12:45:07 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Prints elements of an array
+ * @array: Integer array.
+ * @size: Size of the array.
+ * @action: Function pointer.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
