f1521411254701e724e607122b5ee0c4da390563
SosinaAbi <sosinaabiyot@gmail.com>
2023-04-27 06:07:02 -0700

    Print 'Hello, Holberton' using assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
 global main
 	extern printf
 main:
-	mov edi, format
+	mov edi, msg
 	xor eax, eax
 	call printf
 	mov eax, 0
 	ret
-format: db 'Hello, Holberton\n',0
+msg: db 'Hello, Holberton\n',0
