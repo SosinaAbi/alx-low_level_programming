44aa233af08ca4711608aa74f0daba3ae4be7b45
SosinaAbi <sosinaabiyot@gmail>
2023-06-12 20:04:32 +0000

    Create unkown size array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Creates some size of char array
+ * with the value of char 'c'
+ * @size: size of an array.
+ * @c: element of an array.
+ *
+ * Return: Pointer of an array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *ptr = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
+}
